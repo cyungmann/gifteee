be4f68a2ff76c2ee442ce79e98a14d178eefbf6e
Chris Yungmann <yungmann.chris@gmail.com>
2026-03-22 10:42:38 -0500

    update connection string

--- Api/Program.cs
+++ Api/Program.cs
@@ -17,7 +17,7 @@ builder.Services.Configure<ForwardedHeadersOptions>(options =>
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDbContextFactory<WeatherContext>(opts =>
-    opts.UseNpgsql(builder.Configuration.GetConnectionString("WeatherConnection"), x => x.EnableRetryOnFailure()));
+    opts.UseNpgsql(builder.Configuration.GetConnectionString("POSTGRESQLCONNSTR_gifteee"), x => x.EnableRetryOnFailure()));
 builder.Services.AddTransient<DbInitializer>();
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
